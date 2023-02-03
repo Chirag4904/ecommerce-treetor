830a9e14febee04a3e79e1fc930d4d348f9f7cac
chiggy69 <chiragaggarwal69@gmail.com>
2023-02-03 20:22:25 +0530

    typo fixed while importing

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const { PasswordManager } = require("../services/passwordManager");
+const { PasswordManager } = require("../services/PasswordManager");
 
 const { ifUserExists, addNewUser } = require("../models/user.model");
 
